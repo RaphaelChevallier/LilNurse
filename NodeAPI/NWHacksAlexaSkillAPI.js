ca77ccf9e513ad2cd257f7760c16f3b2baf84073
Raphael Chevallier <raphaelchevallier@hotmail.com>
2019-01-26 19:38:03 -0800

    added node files

---
+++ NodeAPI/NWHacksAlexaSkillAPI.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+app.on('/', (req, res) => {
+  console.log("req: ", req);
+})
+
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+// Create connection to database
+var config =
+{
+    userName: 'LilNurse', // update me
+    password: 'Marpole1', // update me
+    server: 'marpole1.database.windows.net', // update me
+    options:
+    {
+        database: 'Patient-Doctor', //update me
+        encrypt: true
+    }
+}
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err)
+    {
+        if (err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            queryDatabase()
+        }
+    }
+);
+
+function queryDatabase()
+{
+    console.log('Reading rows from the Table...');
+
+    // Read all rows from table
+    var request = new Request(
+        "SELECT * FROM Medication;",
+        function(err, rowCount, rows)
+        {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
